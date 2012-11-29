227695b131a685e8e7e7821a74289967c403238e
BOBTommy <wnsgh611@gmail.com>
2012-11-30 02:16:12 +0900

    GetHeader Using Netty

---
+++ GetHeaderServerHandler.java
@@ -0,0 +1,32 @@
+package HeaderServer;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.jboss.netty.channel.ChannelFuture;
+import org.jboss.netty.channel.ChannelFutureListener;
+import org.jboss.netty.channel.ChannelHandlerContext;
+import org.jboss.netty.channel.ExceptionEvent;
+import org.jboss.netty.channel.MessageEvent;
+import org.jboss.netty.channel.SimpleChannelHandler;
+
+
+public class GetHeaderServerHandler extends SimpleChannelHandler{
+	
+	private static final Logger logger =
+			Logger.getLogger(GetHeaderServerHandler.class.getName());
+	
+	@Override
+	public void messageReceived(
+			ChannelHandlerContext ctx, MessageEvent e) {
+		ChannelFuture f = e.getChannel().write(e.getMessage()); // Print to Browser
+		System.out.println(e.getMessage()); // Print Channel Buffer
+		f.addListener(ChannelFutureListener.CLOSE);
+	}
+	
+	@Override
+	public void exceptionCaught(
+			ChannelHandlerContext ctx, ExceptionEvent e) {
+		logger.log(Level.WARNING,"Error from downstream.",e.getCause());
+		e.getChannel().close();
+	}
+}
