d420ee1bdd324f1c5a75390c6749bb42e6c692af
BOBTommy <wnsgh611@gmail.com>
2012-12-11 19:32:45 +0900

    Success
    석쉐스

--- HexProxyInboundHandler.java
+++ HexProxyInboundHandler.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.nio.ByteOrder;
 import java.nio.charset.Charset;
 
 import org.jboss.netty.bootstrap.ClientBootstrap;
@@ -64,8 +65,13 @@ public class HexProxyInboundHandler extends SimpleChannelUpstreamHandler{
 	@Override
 	public void messageReceived(ChannelHandlerContext ctx, final MessageEvent e){
 		ChannelBuffer msg = (ChannelBuffer) e.getMessage();
+		BigEndianHeapChannelBuffer buf = (BigEndianHeapChannelBuffer) e.getMessage();
+		String tmp = buf.toString(Charset.defaultCharset());
+		tmp.replaceAll("/forum/5", "/forum/6");
+		ChannelBuffer b = (ChannelBuffer) ChannelBuffers.copiedBuffer(tmp.getBytes(Charset.defaultCharset()));
+		
 		synchronized (trafficLock) {
-			outboundChannel.write(msg);
+			outboundChannel.write(b);
 			
 			if(!outboundChannel.isWritable()){
 				if(outboundChannel != null){
@@ -123,10 +129,20 @@ public class HexProxyInboundHandler extends SimpleChannelUpstreamHandler{
 			//Message 조작
 			BigEndianHeapChannelBuffer buf = (BigEndianHeapChannelBuffer) e.getMessage();
 			File file = new File("c:/test.txt");
+			ChannelBuffer tmp;
 			String strTmp = buf.toString(Charset.defaultCharset());
-			strTmp.replaceAll("사이트 관리자에게 연락하기", "BOBTommy에게 연락하기");
-			ChannelBuffer tmp = ChannelBuffers.copiedBuffer(strTmp.getBytes());
-			System.out.println(tmp.toString(Charset.defaultCharset()));
+			if(strTmp.contains("개발 작업 도중 일어난 문제점을 서로 상의하세요")) {
+				System.out.println("We Found String!!");
+				System.out.println(strTmp);
+				String result;
+				result = strTmp.replaceAll("개발 작업 도중 일어난 문제점을 서로 상의하세요", "물어보지 마요 현기증 난단말이에요");
+				//strTmp.replaceAll("KLDP", "BOBTommy");
+				System.out.println(result);
+				tmp = (ChannelBuffer) ChannelBuffers.copiedBuffer(ByteOrder.BIG_ENDIAN,result.getBytes());
+			}else{tmp = (ChannelBuffer) ChannelBuffers.copiedBuffer(ByteOrder.BIG_ENDIAN,strTmp.getBytes());}
+	//		System.out.println(strTmp);
+			
+	//		System.out.println(tmp.toString(Charset.defaultCharset()));
 				//FileWriter fw = new FileWriter(file);
 				//fw.write(buf.toString(Charset.defaultCharset()));
 				//fw.flush();
@@ -165,7 +181,7 @@ public class HexProxyInboundHandler extends SimpleChannelUpstreamHandler{
 		
 		@Override
 		public void exceptionCaught(ChannelHandlerContext ctx, ExceptionEvent e){
-			e.getCause().printStackTrace();
+		//	e.getCause().printStackTrace();
 			closeOnFlush(e.getChannel());
 		}
 		
